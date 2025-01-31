b74898002120c44f6b9b638ccbfee1743c82fd26
SABAREESH B <boopathisabareesh@gmail.com>
2025-01-31 09:49:48 +0530

    Update nodejs3exixt.js

--- nodejs3exixt.js
+++ nodejs3exixt.js
@@ -22,52 +22,52 @@ con.query("USE studentdetails", function (err, result) {
      }
 });
 
-//   var stname = 'poojasri';
-//   var gen = 'female';
-//   var add = 'coimbatore';
-//   var pn = '9743923000';
-//   var maxcb = 'matrix';
-//   var mar12 = '489';
-//   var mar11 = '390';
-//   var mar10 = '360';
-//   var fee = 'paid';
-//   var notfes = 'unpaid';
-//   var hosday = 'hostel';
-//   var stcod = 's136';
+  var stname = 'poojasri';
+  var gen = 'female';
+  var add = 'coimbatore';
+  var pn = '9743923000';
+  var maxcb = 'matrix';
+  var mar12 = '489';
+  var mar11 = '390';
+  var mar10 = '360';
+  var fee = 'paid';
+  var notfes = 'unpaid';
+  var hosday = 'hostel';
+  var stcod = 's136';
 
  
 
-//   var sql_1 = "select * from  details where studentcode = '"+stcod+"';";
-//     con.query(sql_1,function(err,result){
-//           try {
-//               if (err) {
-//                   console.log("error record   :  "+err);
-//               }else if(result.length>0){
+  var sql_1 = "select * from  details where studentcode = '"+stcod+"';";
+    con.query(sql_1,function(err,result){
+          try {
+              if (err) {
+                  console.log("error record   :  "+err);
+              }else if(result.length>0){
 
-//                   console.log(" student code '"+stcod+"' is already exists ")
-//               }
+                  console.log(" student code '"+stcod+"' is already exists ")
+              }
               
-//               else {
-//                   var sql = "insert into details (studentname,gender, address, phonenumber, matrix_or_cbse, mark_12, mark_11, mark_10, fees, notesfees, hostel_dayscholar,studentcode) values('"+stname+"','"+gen+"','"+add+"','"+pn+"','"+maxcb+"','"+mar12+"','"+mar11+"','"+mar10+"','"+fee+"','"+notfes+"','"+hosday+"','"+stcod+"');";
+              else {
+                  var sql = "insert into details (studentname,gender, address, phonenumber, matrix_or_cbse, mark_12, mark_11, mark_10, fees, notesfees, hostel_dayscholar,studentcode) values('"+stname+"','"+gen+"','"+add+"','"+pn+"','"+maxcb+"','"+mar12+"','"+mar11+"','"+mar10+"','"+fee+"','"+notfes+"','"+hosday+"','"+stcod+"');";
                   
-//                   con.query(sql,function(err,result){
-//                       try {
-//                           if (err) {
-//                               console.log("error record   :  "+err);
-//                           } else {
-//                               console.log("inserted sucessfully ");
-//                               console.log("insert record  :  "+JSON.stringify(result));
-//                           }
-//                       } catch (error) {
-//                           console.log("error   :"+error);
-//                       }
-//                   });
+                  con.query(sql,function(err,result){
+                      try {
+                          if (err) {
+                              console.log("error record   :  "+err);
+                          } else {
+                              console.log("inserted sucessfully ");
+                              console.log("insert record  :  "+JSON.stringify(result));
+                          }
+                      } catch (error) {
+                          console.log("error   :"+error);
+                      }
+                  });
                   
-//               }
-//             } catch (error) {
-//               console.log("error   :"+error);
-//             }
-// });
+              }
+            } catch (error) {
+              console.log("error   :"+error);
+            }
+});
 
 
 
